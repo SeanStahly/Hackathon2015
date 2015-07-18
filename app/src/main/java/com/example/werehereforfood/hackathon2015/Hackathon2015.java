ab18440086e6a1dfb50316e6c952442002292e2a
barclata <barclata@gmail.com>
2015-07-18 14:35:53 -0500

    I added something

--- app/src/main/java/com/example/werehereforfood/hackathon2015/Hackathon2015.java
+++ app/src/main/java/com/example/werehereforfood/hackathon2015/Hackathon2015.java
@@ -1,17 +1,32 @@
 package com.example.werehereforfood.hackathon2015;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.*;
 
-public class Hackathon2015 extends ActionBarActivity {
+
+public class Hackathon2015 extends ActionBarActivity implements ConnectionCallbacks, OnConnectionFailedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hackathon2015);
+
+
+        GoogleApiClient myGoogleApi = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
@@ -34,5 +49,20 @@ public class Hackathon2015 extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Location mylastlocation = LocationServices.FusedLocationApi.getLastLocation(myGoogleApi);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
---
+++ app/src/main/java/com/example/werehereforfood/hackathon2015/User.java
@@ -0,0 +1,37 @@
+package com.example.werehereforfood.hackathon2015;
+
+import android.content.Context;
+import android.location.*;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.zza;
+import com.google.android.gms.common.api.zzi;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by TJ on 7/18/2015.
+ */
+public class User  {
+    private String name;
+    private User huntedBy;
+    private User isHunting;
+
+    public String getName() {
+        return name;
+    }
+};
+
+}
